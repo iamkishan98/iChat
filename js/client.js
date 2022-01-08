c33591c96aa53c392190acd94c7954fd913d7fc1
Gopikishan Thawre <gopikishanthawre@gmail.com>
2022-01-08 17:37:31 +0530

    Just simple chat app

---
+++ js/client.js
@@ -0,0 +1,35 @@
+const socket = io('http://localhost:8000');
+
+const form = document.getElementById('send-container');
+const message = document.getElementById('msginput');
+const msgcontainer = document.querySelector(".container");
+
+const user = prompt("Enter your name : ");
+var audio = new Audio('ting.mp3');
+
+const append = (msg,pos)=>{
+    const msgelement = document.createElement('div');
+    msgelement.innerText = msg;
+    msgelement.classList.add('msg');
+    msgelement.classList.add(pos);
+    msgcontainer.append(msgelement);
+    audio.play();
+    
+};
+
+form.addEventListener('submit',(event)=>{
+    event.preventDefault();
+    const msg = msginput.value;
+    append(`You: ${msg}`,'right');
+    socket.emit('send',msg);
+    msginput.value='';
+});
+socket.emit("new-user-joined", user);
+
+socket.on('user-joined', (user)=>{
+    append(`${user} joined the chat`,'left');
+});
+
+socket.on('receive', (data)=>{
+    append(`${data.name}: ${data.message}`,'left');
+});
---
+++ nodeServer/index.js
@@ -0,0 +1,36 @@
+const cors = require('cors');
+const httpServer = require('http').createServer();
+
+const io = require("socket.io")(8000,{
+     cors: { origin: '*' } 
+});
+
+/*
+httpServer.listen(8000,()=>{
+    console.log("Listening to port ");
+});
+*/
+const users= {};
+
+
+io.on('connection', (socket)=>{
+    console.log(socket.id);
+    //socket.emit('new-user-joined',"Gopi");
+
+    socket.on('new-user-joined',(name)=>{
+        console.log("name :", name);
+        users[socket.id] = name;
+
+        socket.broadcast.emit('user-joined',name);
+        
+    });
+
+    socket.on('send',(message)=>{
+        console.log(message);
+        socket.broadcast.emit('receive', {message: message, name:users[socket.id] } );
+    });
+});
+
+
+
+
